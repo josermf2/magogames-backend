5ba3d42f81dbb0e28eb4136201c2e7e3ab6c092c
Lucca Barufatti <luccabvs@al.insper.edu.br>
2021-12-02 21:44:06 -0300

    aplica pesquisa e filtro

--- magogames_backend/urls.py
+++ magogames_backend/urls.py
@@ -7,5 +7,11 @@ urlpatterns = [
     path('externalAPI/home', views.get_homepageDeals),
     path('externalAPI/store', views.get_store),
     path('externalAPI/gameLookup/<int:id>', views.get_gameLookup),
-
+    path('externalAPI/search/<str:title>', views.get_search),
+    path('externalAPI/search/min/<str:title>/<int:minprice>', views.get_search),
+    path('externalAPI/search/max/<str:title>/<int:maxprice>', views.get_search),
+    path('externalAPI/search/min/<int:minprice>', views.get_search),
+    path('externalAPI/search/max/<int:maxprice>', views.get_search),
+    path('externalAPI/search/<int:minprice>/<int:maxprice>', views.get_search),
+    path('externalAPI/search/<str:title>/<int:minprice>/<int:maxprice>', views.get_search),
 ]
--- magogames_backend/views.py
+++ magogames_backend/views.py
@@ -19,3 +19,29 @@ def get_store(request):
 def get_gameLookup(request, id):    
     r = requests.get('https://www.cheapshark.com/api/1.0/games?id='+str(id)).json()   
     return Response(r) 
+
+@api_view(['GET'])
+def get_search(request, title='', minprice='', maxprice=''):
+    if title != '':
+        if minprice != '' and maxprice != '':
+            r = requests.get('https://www.cheapshark.com/api/1.0/deals?title='+str(title)+'&lowerPrice='+str(minprice)+'&upperPrice='+str(maxprice)).json()
+            return Response(r)
+        elif minprice != '':
+            r = requests.get('https://www.cheapshark.com/api/1.0/deals?title='+str(title)+'&lowerPrice='+str(minprice)).json()
+            return Response(r)
+        elif maxprice != '':
+            r = requests.get('https://www.cheapshark.com/api/1.0/deals?title='+str(title)+'&upperPrice='+str(maxprice)).json()
+            return Response(r) 
+        else:
+            r = requests.get('https://www.cheapshark.com/api/1.0/deals?title='+str(title)).json()
+            return Response(r)
+    else:
+        if minprice != '' and maxprice != '':
+            r = requests.get('https://www.cheapshark.com/api/1.0/deals?&lowerPrice='+str(minprice)+'&upperPrice='+str(maxprice)).json()
+            return Response(r)
+        elif minprice != '':
+            r = requests.get('https://www.cheapshark.com/api/1.0/deals?lowerPrice='+str(minprice)).json()
+            return Response(r)
+        elif maxprice!= '':
+            r = requests.get('https://www.cheapshark.com/api/1.0/deals?upperPrice='+str(maxprice)).json()
+            return Response(r)
